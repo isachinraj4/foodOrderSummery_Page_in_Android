9a828121444ad81fec69d164c768d207687bd417
Sachin <isachinraj@outlook.com>
2022-03-10 00:10:33 +0530

    first commit

---
+++ app/src/main/java/com/example/android/foodorderapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.android.foodorderapp;
+
+import android.icu.text.NumberFormat;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void submitOrder(View view) {
+        int quantity = 5;
+        displayQuantity(quantity);
+        displayPrice(quantity*40);
+    }
+
+    private void displayQuantity(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+}
